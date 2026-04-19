f6934f5c25e072ac625a32e73eb51f77abb06299
work3g-hash <work3g@gmail.com>
2026-04-19 20:18:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import os
+import tempfile
 from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
@@ -23,6 +24,49 @@ ADMIN_ID = 691846456
 GROUP_LINK = "https://t.me/+tLpqhb4CvOYyMjk6"
 SHEET_ID = "1DfHKP3uXaEy4ZaKZRAeghPGlvLX7BWwnl6cVg7gyKPc"
 
+GOOGLE_CREDS = {
+    "type": "service_account",
+    "project_id": "women-club-bot",
+    "private_key_id": "f02e40f804bb801528290c7e2509b219bda303b6",
+    "private_key": (
+        "-----BEGIN PRIVATE KEY-----\n"
+        "MIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQDKByBCmRwE5cHA\n"
+        "ZY+Qj2P02+Yqo24VgUZDQc+azAOIoRCsZfeo+qnmzuQPCg1zb7z3lbJPNKtOkPM9\n"
+        "8WWXfFpi6WSmE6nwUlV/MJ4BVsRsPB+kKcWkrsjHtd8xVVinOR50Su4j9/g0oDPC\n"
+        "xhIP4ZHZY3VocRelaAdWx3bvaBTA11PNTGabSTtjEOFwozfIcc9kT4g8F2kWMFGj\n"
+        "p7ndlwFteQqk7PR9XffrzGmiUcWvUwg6nSLpFLc4OW/194mvITxOHKKpGmVDP7CT\n"
+        "1PhtodV5HZZ9oW+sEgB58yJ8s/z6VH616UZ6t064iJPWLeLvzaY11gOFRO38P5tx\n"
+        "PB0wNt+JAgMBAAECggEAHEjuH9Te4uKmA0FU3cjtljEZYvvZpEIiq7Txk3sfvYfL\n"
+        "VU+7Ylmh4vIxZNej9Xb97m3zdnppx4isvY3oCtL6tqF1mXjkUIS+ep28aXST8JFk\n"
+        "XrAIf1uHOcrBUl9gF518IY0CFWAYzIlnZcwkaDvNFzIBFJvM0zgi2uf3Q2J/kVX0\n"
+        "iNZ+lFpS3lNTc4TQFbbdtjT291JZy/OtffXeas7fWGGfSYSh+zGWNibgA9hehbuD\n"
+        "wr4uxYQOrm2nranL0fr7nx+l+wkwKGuls9OlgH1unbGCNGJ2qu3IjLWB4yZrRRDL\n"
+        "KyViiIHMjMJcDMPO0Bokvd+VwyUCB8MfvS0+hB0aeQKBgQD0lAPCkkbFj0GL99ps\n"
+        "HGYUre0Tr/yncv/evNoH5dA5CwRXIIA0qmRa1R6oYKgaZ3ohjWzwFOm0lYD+PM4/\n"
+        "FEOf+3dI+/KQVGY3vLxIhO2ReylXDi3AiwvnRrsaAth1QHYFIvh4+DPWzvL61Zcl\n"
+        "ufheIsj0F0kEs5+T8lI2wwI53QKBgQDTdmmmM6Tj4FTtWhYpFPBOKfiRhx2OnPfa\n"
+        "vjLIHag6QMDLetRNNtQh0Wnh74b6h2Otg/Bq+Qd5QShLPWHGtkwX5ANp3g9tbvk4\n"
+        "IEFmCfaYhtR1STzUmistvCcC3Rc2GpkQ723OMkbW2xe3aQYhFQ0oC1dlpjzUoT8T\n"
+        "TZ05dkw/nQJ/awBWpMlaLRR/mLzW0nWaM3HkRri30Ip/ZvM4cDwa3Nn1DCkr7d6e\n"
+        "CR80SnX/FY4v4H3/Kwn7NQYzaQcxNGepLlTV2xhfBsXl8nyf4xpE1WEMtQl++r3a\n"
+        "d1R6hua3zJRnDdg+3K26AECKDTNk7RxvjL+rKx8E0wnCxvc9ALhQQQKBgGiF3U7j\n"
+        "tBtXJWN05gNEcEuSf2UfjkKR4AllfiBgWGkC3Mk3W30XHt/gbR/aj7OB/Ikl8E5P\n"
+        "7ZvH0yztmEjqjs44TF+l/aYv9kwB1ZGkVxmpe5bFrqW/1pvypq3JrtF1cDdowbPs\n"
+        "Mgu9nAlyhi8QAsLKaFa8RtErKsxVzuM6UBIZAoGBAMA2E4A+/zbT7Rtitj1IJMBM\n"
+        "y5gtBRFoplWtfadw6WUr+I0ieqaXZzZ3AaZQtbDc9bkI4I6xLxcZ++7xgN6ITRhL\n"
+        "w0k0GFgsxgyVZ1zIweI2WVhRLll5Q4WGt71av+4BZx8jFh6LaEnszkOJAQ3FgTzL\n"
+        "pvqKLegPSTnsoqKgqB6m\n"
+        "-----END PRIVATE KEY-----\n"
+    ),
+    "client_email": "women-club-bot@women-club-bot.iam.gserviceaccount.com",
+    "client_id": "103045299427313456408",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/women-club-bot%40women-club-bot.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
 NAME, CITY, WORK, VALUE, WHY, RULES = range(6)
 
 RULES_TEXT = """📋 Правила нашего клуба:
@@ -36,10 +80,8 @@ RULES_TEXT = """📋 Правила нашего клуба:
 
 
 def get_sheet():
-    creds_json = os.environ.get("GOOGLE_CREDS_JSON")
-    creds_dict = json.loads(creds_json)
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-    creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+    creds = Credentials.from_service_account_info(GOOGLE_CREDS, scopes=scopes)
     client = gspread.authorize(creds)
     sheet = client.open_by_key(SHEET_ID).sheet1
     if not sheet.get_all_values():
